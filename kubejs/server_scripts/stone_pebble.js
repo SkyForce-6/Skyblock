1c43ecfb30f21cac44c9d3f9fc950d854093f6ac
SkyForce <33234261+SkyForce-6@users.noreply.github.com>
2025-03-19 17:13:06 +0100

    Delete kubejs/server_scripts/stone_pebble.js

--- kubejs/server_scripts/stone_pebble.js
+++
@@ -1,8 +0,0 @@
-BlockEvents.broken(event => {
-
-  if (event.block.id === 'minecraft:stone' && event.player.mainHandItem.id === 'minecraft:air') {
-    
-    event.player.give('exdeorum:stone_pebble')
-    
-  }
-})
